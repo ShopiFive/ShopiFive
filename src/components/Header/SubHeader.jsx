c982a19519d86b5c62b08d7b73f714e1882275d0
Sheila Ho <ho.sheila@hotmail.com>
2023-05-20 19:34:24 -0400

    feat: added banner to subheader
    fixed mobile nav icon size

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AlphaCard, Text, Link } from "@shopify/polaris";
+import { AlphaCard, Text, Link, Banner, Button } from "@shopify/polaris";
 import "./Header.scss";
 
 export default function SubHeader({ text, link }) {
@@ -13,6 +13,15 @@ export default function SubHeader({ text, link }) {
           <div className="subheader__items">
             <Link url="#">{link}</Link>
           </div>
+          <Banner onDismiss={() => {}}>
+            <div className="subheader__banner">
+              <p className="subheader__bannertext">
+                Extend your trial for <i>free</i> up to 3 months by creating a
+                business plan.
+              </p>
+              <Button>Lean More</Button>
+            </div>
+          </Banner>
         </div>
       </AlphaCard>
     </div>

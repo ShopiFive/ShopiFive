343deefdcf18be63b4a43cdd5f600212829c5fbc
Sheila Ho <ho.sheila@hotmail.com>
2023-05-20 20:02:37 -0400

    feat: linked learnmore button to myplan
    added button color green to variables and fixed button style

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { AlphaCard, Text, Link, Banner, Button } from "@shopify/polaris";
 import "./Header.scss";
 
 export default function SubHeader({ text, link }) {
+  const navigate = useNavigate();
+
+  const handleButtonClick = () => {
+    navigate("/my-plan");
+  };
+
   return (
     <div className="subheader">
       <AlphaCard>
@@ -19,7 +26,7 @@ export default function SubHeader({ text, link }) {
                 Extend your trial for <i>free</i> up to 3 months by creating a
                 business plan.
               </p>
-              <Button>Lean More</Button>
+              <Button onClick={handleButtonClick}>Lean More</Button>
             </div>
           </Banner>
         </div>

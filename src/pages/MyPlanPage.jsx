72317ee4686d87be37321a956575c836f4cce3a9
Sheila Ho <ho.sheila@hotmail.com>
2023-05-21 17:12:07 -0400

    feat: added form pop up

--- src/pages/MyPlanPage.jsx
+++ src/pages/MyPlanPage.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Header from "../components/Header/Header";
-import HowItWorks from "../components/HowItWorks/HowItWorks";
+import MyBusinessPlan from "../components/MyBusinessPlan/MyBusinessPlan";
+import SearchBar from "../components/Search/SearchBar";
 
 export default function MyPlanPage() {
   return (
     <>
+      <SearchBar />
       <Header
         titleName="My Business Plan"
         text="Subhead here"
         color="subdued"
       />
-      <HowItWorks />
+      <MyBusinessPlan />
     </>
   );
 }

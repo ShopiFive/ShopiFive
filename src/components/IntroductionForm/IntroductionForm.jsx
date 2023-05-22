21c2bb6866ea789e656bdbc6d8f85551a832e383
Sheila Ho <ho.sheila@hotmail.com>
2023-05-22 17:49:09 -0400

    chore: removed ununsed code

--- src/components/IntroductionForm/IntroductionForm.jsx
+++ src/components/IntroductionForm/IntroductionForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Box, ProgressBar } from "@shopify/polaris";
 import { InfoMinor } from "@shopify/polaris-icons";
 import "./IntroductionForm.scss";

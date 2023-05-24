9089f5ca91011e22938c35d8fd248405cd1be20a
Elizabeth-AA <elizabeth2awesome@gmail.com>
2023-05-23 17:27:35 -0700

    fix: got the current components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import { AlphaCard, Divider, Text } from "@shopify/polaris";
 import "./Header.scss";
 
-export default function Header({ titleName, text, color }) {
+export default function Header({ titleName, text, color, showDivider }) {
   return (
     <div className="header">
       <AlphaCard>
         <Text variant="heading4xl" as="h1">
           {titleName}
         </Text>
-        <Text color={color} as="p">
-          {text}
-        </Text>
+        <p className="header__subtext">{text}</p>
       </AlphaCard>
-      <Divider color="subdued" />
+      {showDivider && <Divider color="subdued" />}
     </div>
   );
 }

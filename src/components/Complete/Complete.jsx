dd7e47f0cf9d830668a849be5dd1eb1ed925d857
Sheila Ho <ho.sheila@hotmail.com>
2023-05-24 20:09:33 -0400

    chore: removed unused code

--- src/components/Complete/Complete.jsx
+++ src/components/Complete/Complete.jsx
@@ -1,12 +1,8 @@
 import React from "react";
+import "./Complete.scss";
+
 import SearchBar from "../Search/SearchBar";
 import Header from "../Header/Header";
-import "./Complete.scss";
-import { Text, Icon, Button, Link } from "@shopify/polaris";
-// import { ClockMajor } from "@shopify/polaris-icons";
-import complete from "../../assets/images/complete-image.jpg";
-import calender from "../../assets/images/calendar.png";
-import clock from "../../assets/images/clock.png";
 import CompleteInfo from "./CompleteInfo";
 
 export default function Complete({ onNext }) {

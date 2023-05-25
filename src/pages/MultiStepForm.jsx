dafb849be3832f537ff058f47e7d79c64e9f6484
Sheila Ho <ho.sheila@hotmail.com>
2023-05-24 20:28:37 -0400

    chore: removed comments

--- src/pages/MultiStepForm.jsx
+++ src/pages/MultiStepForm.jsx
@@ -14,17 +14,14 @@ export default function MultiStepForm() {
   console.log(formData);
 
   const handleNext = (data) => {
-    // Update the form data with the current step's data
     setFormData((prevData) => ({
       ...prevData,
       step1: data,
     }));
 
-    // Proceed to the next step
     setStep((prevStep) => prevStep + 1);
   };
 
-  // Render the appropriate step based on the current step value
   const renderStep = () => {
     switch (step) {
       case 1:
@@ -41,7 +38,6 @@ export default function MultiStepForm() {
         return <Finish onNext={handleNext} />;
       case 7:
         return <Complete onNext={handleNext} />;
-      // Add more cases for other steps
       default:
         return null;
     }
